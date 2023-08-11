04953dd114156d79ba27bf2ebdcfefbc995042e7
pakitowka <whitesaint12@gmail.com>
2023-08-11 21:40:45 +0200

    Implementing class with definitions searching

--- definition.py
+++ definition.py
@@ -1,3 +1,5 @@
+import pandas
+
 class Definition:
     """
     This class represents definitions of the words.
@@ -6,5 +8,16 @@ class Definition:
     def __init__(self, term):
         self.term = term
 
-    def get(self):
-        pass
+    def get_definition(self):
+        """gets definition of the word from dataFrame"""
+        df = pandas.read_csv('files/data.csv')
+        word_definitions = tuple(df.loc[df['word'] == self.term]['definition'])
+        return word_definitions
+
+
+if __name__ == '__main__':
+    term = input('Type word: ')
+    definition = Definition(term=term)
+    definitions = definition.get_definition()
+    for i in definitions:
+        print(i)
