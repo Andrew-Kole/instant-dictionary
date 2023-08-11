d5981bc7592cb4831ada0683775984ace589703b
pakitowka <whitesaint12@gmail.com>
2023-08-11 21:16:03 +0200

    App structure

---
+++ definition.py
@@ -0,0 +1,10 @@
+class Definition:
+    """
+    This class represents definitions of the words.
+    """
+
+    def __init__(self, term):
+        self.term = term
+
+    def get(self):
+        pass
