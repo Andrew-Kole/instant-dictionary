680f51f51671348d8102c02c71e96e4b08d3077d
pakitowka <whitesaint12@gmail.com>
2023-08-27 01:30:25 +0200

    instant dictionary

--- definition.py
+++ definition.py
@@ -1,23 +1,24 @@
 import pandas
 
+
 class Definition:
     """
     This class represents definitions of the words.
     """
 
-    def __init__(self, term):
-        self.term = term
+    def __init__(self, word):
+        self.word = word
 
     def get_definition(self):
         """gets definition of the word from dataFrame"""
         df = pandas.read_csv('files/data.csv')
-        word_definitions = tuple(df.loc[df['word'] == self.term]['definition'])
+        word_definitions = tuple(df.loc[df['word'] == self.word]['definition'])
         return word_definitions
 
 
 if __name__ == '__main__':
     term = input('Type word: ')
-    definition = Definition(term=term)
+    definition = Definition(word=term)
     definitions = definition.get_definition()
     for i in definitions:
         print(i)
